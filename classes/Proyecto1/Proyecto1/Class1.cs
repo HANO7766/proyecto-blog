b9b1278f73ff4364a093c11228dc365fcade7d4f
renat <renat@LAPTOP-J1AFEOCC>
2022-06-01 17:15:44 -0400

    v3.0

---
+++ classes/Proyecto1/Proyecto1/Class1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Proyecto1
+{
+    public class cliente
+    {
+        public string nombre_cliente;
+        public string 
+
+
+        public string Nombre_cliente { get => nombre_cliente; set => nombre_cliente = value; }
+    }
+}
