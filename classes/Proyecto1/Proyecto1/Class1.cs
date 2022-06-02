b58c75069f0233a8e41051439b16e69ac1d1bd55
renat <renat@LAPTOP-J1AFEOCC>
2022-06-02 16:12:54 -0400

    test4.0

--- classes/Proyecto1/Proyecto1/Class1.cs
+++ classes/Proyecto1/Proyecto1/Class1.cs
@@ -6,21 +6,38 @@ using System.Threading.Tasks;
 
 namespace Proyecto1
 {
-    public class cliente
+    public class Cliente
 
     {
         public int cliente_id;
         public String cliente_nombre;
-        public String cliente_direccion; 
+        private String cliente_direccion;
+        private String cliente_correo;
+        private String cliente_domicilio;
+        private String cliente_datosTarjeta;
+
 
 
 
 
         public string Nombre_cliente { get => cliente_nombre; set => cliente_nombre = value; }
+        public string Cliente_direccion { get => cliente_direccion; set => cliente_direccion = value; }
+        public string Cliente_correo { get => cliente_correo; set => cliente_correo = value; }
+        public string Cliente_domicilio { get => cliente_domicilio; set => cliente_domicilio = value; }
+        public string Cliente_datosTarjeta { get => cliente_datosTarjeta; set => cliente_datosTarjeta = value; }
 
-        public cliente()
+        public Cliente()
         {
         }
 
+        public Cliente(int cliente_id, string cliente_nombre, string cliente_direccion, string cliente_correo, string cliente_domicilio, string cliente_datosTarjeta)
+        {
+            this.cliente_id = cliente_id;
+            this.cliente_nombre = cliente_nombre;
+            this.cliente_direccion = cliente_direccion;
+            this.cliente_correo = cliente_correo;
+            this.cliente_domicilio = cliente_domicilio;
+            this.cliente_datosTarjeta = cliente_datosTarjeta;
+        }
     }
 }
